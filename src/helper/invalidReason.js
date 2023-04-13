4258feb1ffcf2cb9f4ecbb4807b01c21f23ff8e7
marifeta <marifeta1@mail.ru>
2023-04-13 19:56:21 +0300

    remove files for github pages

--- src/helper/invalidReason.js
+++
@@ -1,8 +0,0 @@
-export const InvalidReason = Object.freeze({
-    Empty: Symbol('Поле не может быть пустым'),
-    Miss: Symbol('Пароли должны совпадать'),
-    Length: Symbol('Минимальная длина пароля - 8 символов'),
-    Reg: Symbol('Кириллические символы запрещены')
-})
-
-
